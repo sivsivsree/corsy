b73c33e82d58fd92f00f0b620f4588f08d1dc063
siv s <sivsivsree@gmail.com>
2021-07-16 09:58:54 +0000

    Brew formula update for corsy version v0.0.4

--- Formula/corsy.rb
+++ Formula/corsy.rb
@@ -5,25 +5,25 @@
 class Corsy < Formula
   desc "Corsy is a proxy injector for development to avoid CORS issues while building SPA applications.."
   homepage "https://github.com/sivsivsree/corsy#corsy"
-  version "0.0.3"
+  version "0.0.4"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sivsivsree/corsy/releases/download/v0.0.3/corsy_0.0.3_darwin_amd64.tar.gz", :using => CurlDownloadStrategy.
-      sha256 "fe70ad7c8cd50b2f6e62aeda7e5910a5b1391508966ab6f91143414e24d9fca1"
+      url "https://github.com/sivsivsree/corsy/releases/download/v0.0.4/corsy_0.0.4_darwin_amd64.tar.gz"
+      sha256 "8d835dfb997aa717080fb7a995a9ae58504c19e71a29b6648fa361c67394d0f4"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/sivsivsree/corsy/releases/download/v0.0.3/corsy_0.0.3_linux_amd64.tar.gz", :using => CurlDownloadStrategy.
-      sha256 "af598d5e3ec5b878f194e8261ebe32a2ac9fdea0d909f5ad87a49772b3c256b6"
+      url "https://github.com/sivsivsree/corsy/releases/download/v0.0.4/corsy_0.0.4_linux_amd64.tar.gz"
+      sha256 "9d46883c3a0f2b02a52ac52303454b021753b308db0df96ea06eb3de9cf80db5"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sivsivsree/corsy/releases/download/v0.0.3/corsy_0.0.3_linux_arm64.tar.gz", :using => CurlDownloadStrategy.
-      sha256 "6106c530b67fb7eeb7a99a419f99b82cea1963dd91d6ac5cd36a81ccbf221eab"
+      url "https://github.com/sivsivsree/corsy/releases/download/v0.0.4/corsy_0.0.4_linux_arm64.tar.gz"
+      sha256 "7c897882290792c48edc9fee906347a1ad6e3032f827fdb0337f1b78b26ec94f"
     end
   end
 
